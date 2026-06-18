02f6636653f16054b67386625d5d489a59070adb
ayush130305 <41382957+ayush130305@users.noreply.github.com>
2026-06-18 11:02:04 +0530

    Update imem.v

--- imem.v
+++ imem.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
 module imem(
-    input [7:0] addr,
-    output[7:0] instr
+    input [7:0] addr, //address stored
+    output[7:0] instr //has instructions, like opcodes are decoded to this 
     );
-    reg [7:0] rom [255:0];
+    reg [7:0] rom [255:0]; //created a memory array
     
 //    initial begin
 //    $readmemh("program.mem", rom);
