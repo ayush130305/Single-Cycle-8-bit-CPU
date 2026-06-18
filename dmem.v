6a2e01f814578a401e32e0e57e7edf8f27e820cf
ayush130305 <41382957+ayush130305@users.noreply.github.com>
2026-06-18 10:57:47 +0530

    Enhance dmem.v with comments for clarity
    Added comments to clarify the purpose of inputs and memory array.

--- dmem.v
+++ dmem.v
@@ -3,14 +3,14 @@
 
 module dmem(
     input clk,
-    input mem_read,
-    input mem_write,
-    input [7:0] addr,
-    input [7:0] wr_data,
-    output [7:0] rd_data
+    input mem_read, //used for LD, puts mem[addr] <- data inside onto address to be loaded from reg
+    input mem_write, // used for ST, purs mem[addr] data into a register 
+    input [7:0] addr, //address
+    input [7:0] wr_data, // after mem_wr is 1 it allows data in wr_data to be written to mem[addr]
+    output [7:0] rd_data // if mem_rd is 1 it allows mem[addr] to be accessed to be read
     );
 
-reg [7:0] mem [7:0];
+    reg [7:0] mem [7:0]; //mem array in register
 assign rd_data = mem_read ? mem[addr] : 8'b0;
 
 always @(posedge clk)
