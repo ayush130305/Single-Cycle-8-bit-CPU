83fe63a87b7468d204bf977384081c60e28fcea8
ayush130305 <41382957+ayush130305@users.noreply.github.com>
2026-06-18 11:03:58 +0530

    Update imem.v

--- imem.v
+++ imem.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
 module imem(
-    input [7:0] addr, //address stored
-    output[7:0] instr //has instructions, like opcodes are decoded to this 
+    input [7:0] addr, //address from PC — selects which instruction to fetch
+    output[7:0] instr //fed into decoder to extract opcode, registers, immediate
     );
-    reg [7:0] rom [255:0]; //created a memory array
+    reg [7:0] rom [255:0]; //created a memory array of 256, with 8 bit entries
     
 //    initial begin
 //    $readmemh("program.mem", rom);
